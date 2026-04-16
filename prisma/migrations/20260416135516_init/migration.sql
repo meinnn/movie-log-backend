2cbe63514347868d91c124409e8a2c2cc2984038
meinnn <hyein123k@gmail.com>
2026-04-16 23:23:50 +0900

    feat: 프로젝트 초기 세팅 (NestJS + Prisma)

---
+++ prisma/migrations/20260416135516_init/migration.sql
@@ -0,0 +1,30 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "nickname" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Record" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "tmdbMovieId" INTEGER NOT NULL,
+    "watchedAt" TIMESTAMP(3) NOT NULL,
+    "rating" DOUBLE PRECISION NOT NULL,
+    "memo" TEXT,
+    "quote" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Record_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Record" ADD CONSTRAINT "Record_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
